1ec5e499c0c4da9b4ae6ddfd1f801167442d4914
Typ3kq <kacpertywonek60@gmail.comm>
2024-05-04 09:38:42 +0200

    zad4

--- Main.java
+++ Main.java
@@ -1,12 +1,30 @@
+import company.beans.Person;
+import company.exceptions.InvalidAgeException;
 import company.implementations.EmailMessenger;
 import company.utils.MathUtils;
 
 public class Main {
     public static void main(String[] args) {
-        int result = MathUtils.add(10, 5);
-        System.out.println("Result of addition: " + result);
+        Person[] people = new Person[5];
+        final int b = 10;
+
+        try {
+            people[0] = new Person("John Doe", 30);
+            people[1] = new Person("Emily Johnson", 25);
+            people[2] = new Person("Nathan Patel", 40);
+            people[3] = new Person("Lily Thompson", 35);
+            people[4] = new Person("Marcus Brown", 28);
+        } catch (InvalidAgeException e) {
+            System.out.println("Invalid age provided: " + e.getMessage());
+        }
 
         EmailMessenger emailMessenger = new EmailMessenger();
-        emailMessenger.sendMessage("The result of addition is: " + result);
+
+        for (Person person : people) {
+            if (person != null) {
+                int value = MathUtils.add(person.getAge(), b);
+                emailMessenger.sendMessage("Hello " + person.getName() + ", the value is: " + value);
+            }
+        }
     }
 }
